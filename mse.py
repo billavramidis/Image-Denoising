9104b501b79d1e6825759f3c8c722e19b981035a
billavramidis <bavramidis04@gmail.com>
2025-12-24 19:10:38 +0200

    feat: added checking for importing files

--- mse.py
+++ mse.py
@@ -10,19 +10,25 @@ denoised_image_paths = [
 ]
 
 clean_directory_path = Path("inputs/clean/")
-clean_image_path = [
-    f for f in clean_directory_path.iterdir() if f.suffix in [".jpg", ".png", ".webp"]
+clean_image_paths = [
+    f
+    for f in clean_directory_path.iterdir()
+    if f.suffix.lower() in [".jpg", ".png", ".webp"]
 ]
-clean_image = clean_image_path[0]
+
+if len(clean_image_paths) == 1:
+    clean_image = clean_image_paths[0]
+else:
+    raise ValueError(f"Expected 1 Image, found {len(clean_image_paths)}")
 
 
 for denoised_image in denoised_image_paths:
     with Image.open(denoised_image) as img1, Image.open(clean_image) as img2:
         if not img1.size == img2.size:
-            raise ValueError(f"Image Dimensions Do Not Match")
+            raise ValueError("Image Dimensions Do Not Match")
 
         denoised_pixels = np.array(img1.convert("RGB")).astype(np.float64)
         clean_pixels = np.array(img2.convert("RGB")).astype(np.float64)
 
         mse = np.mean((clean_pixels - denoised_pixels) ** 2)
-        print(f"MSE: {mse}")
+        print(f"MSE for {denoised_image.stem}: {round(mse,3)}")
